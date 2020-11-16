7e5218fbca5b9cd2f6c4177c2ad8382a25b80e7e
caroMaio <carolinabelenmaio@gmail.com>
2020-11-16 11:06:34 -0300

    [ADD] Responsive styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const questionText = document.querySelectorAll("#question-p");
-const answer = document.querySelectorAll("#answer");
-const arrow = document.querySelectorAll("#img-arrow");
+const questionText = document.querySelectorAll(".question-p");
+const answer = document.querySelectorAll(".answer");
+const arrow = document.querySelectorAll(".img-arrow");
+let questionSelected = 0;
 
 function show() {
     for (let i = 0; i < questionText.length; i++){
@@ -9,6 +10,7 @@ function show() {
             ? hiddenQuestion(questionText[i], arrow[i], answer[i]) 
             : showQuestion(questionText[i], arrow[i], answer[i]);
         }
+       
     }
 
 }
@@ -18,13 +20,14 @@ function showQuestion (text, img, paragraph) {
     text.classList.add('question-open-text');
     img.classList.add('question-open');
     paragraph.classList.remove('hidden');
+    questionSelected++;
 };
 
 function hiddenQuestion(text, img, paragraph) {
     text.classList.remove('question-open-text');
     img.classList.remove('question-open');
     paragraph.classList.add('hidden');
-}
+};
 
 
 show()
